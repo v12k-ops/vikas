b661bf1075135cc97119e210b3b988e59923c7d1
Vikas Repo <Vikasvkulkarni@gmail.com>
2025-10-08 23:03:45 +0530

    Update main

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  subscription_id = "70d13209-b5da-483b-9f07-a91b9e1a684b"
+  # subscription_id = "70d13209-b5da-483b-9f07-a91b9e1a684b"
 }
 
-resource "azurerm_resource_group" "rg-actions" {
-  name     = "dhondu-rg"
+resource "azurerm_resource_group" "rg-actions1" {
+  name     = "dhondu-rg-123"
   location = "south india"
 }
